92a764e326a36e6ffe87971fdeb6cfbb1b7956cd
simo14 <s.morenou@alumnos.urjc.es>
2016-06-13 10:21:11 +0200

    GitIgnore dist folder

--- dist/dev/app/shared/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./elastic.service'));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9zaGFyZWQvc2VydmljZXMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUdBLGlCQUFjLG1CQUFtQixDQUFDLEVBQUEiLCJmaWxlIjoiYXBwL3NoYXJlZC9zZXJ2aWNlcy9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogVGhpcyBiYXJyZWwgZmlsZSBwcm92aWRlcyB0aGUgZXhwb3J0IGZvciB0aGUgc2hhcmVkIE5hdmJhckNvbXBvbmVudC5cbiAqL1xuZXhwb3J0ICogZnJvbSAnLi9lbGFzdGljLnNlcnZpY2UnO1xuIl19
